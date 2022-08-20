0c015fc70a07ee1be8add6ebc4d01f40676afeeb
cookiefu0225 <frankyang6668@gmail.com>
2022-08-20 15:24:11 -0500

    Install DjangoRestFramework and Implement Swagger and OpenAPI Schema

--- app/app/settings.py
+++ app/app/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
+# Enabling apps
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -39,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'rest_framework',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -131,3 +134,8 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.User'
+
+# Tell Rest how to use this schema
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
